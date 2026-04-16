583fa57f151cd98f005a79da010b78f614aa4d5e
Ozgur <ozgurozvaris@hotmail.com>
2026-04-16 17:02:00 +0300

    Implemented Students View

---
+++ SchoolApp-MVC/Controllers/StudentsController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace SchoolApp_MVC.Controllers
+{
+    public class StudentsController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
